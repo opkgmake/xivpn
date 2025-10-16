1a2b4631fdc4da37a51a1c4f4cfba4e14a349600
tn rf <90899614+opkgmake@users.noreply.github.com>
2025-10-16 15:56:10 +0800

    feat: localize WireGuard editor and add keepalive

--- app/src/main/java/cn/gov/xivpn2/ui/WireguardActivity.java
+++ app/src/main/java/cn/gov/xivpn2/ui/WireguardActivity.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Type;
 import java.util.LinkedHashMap;
 import java.util.regex.Pattern;
 
+import cn.gov.xivpn2.R;
 import cn.gov.xivpn2.crypto.Key;
 import cn.gov.xivpn2.crypto.KeyFormatException;
 import cn.gov.xivpn2.xrayconfig.Outbound;
@@ -27,6 +28,16 @@ public class WireguardActivity extends ProxyActivity<WireguardSettings> {
                 return !value.isEmpty();
             case "RESERVED_PATTERN":
                 return RESERVED_PATTERN.matcher(value).find();
+            case "PEER_PERSISTENT_KEEPALIVE":
+                if (value.isEmpty()) {
+                    return true;
+                }
+                try {
+                    int keepAlive = Integer.parseInt(value);
+                    return keepAlive >= 0 && keepAlive <= 65535;
+                } catch (NumberFormatException ignored) {
+                    return false;
+                }
         }
         return super.validateField(key, value);
     }
@@ -57,6 +68,10 @@ public class WireguardActivity extends ProxyActivity<WireguardSettings> {
         peer.publicKey = adapter.getValue("PEER_PUBLIC_KEY");
         peer.preSharedKey = adapter.getValue("PEER_PRE_SHARED_KEY");
         peer.allowedIPs = adapter.getValue("PEER_ALLOWED_IPS").split(",");
+        String keepAliveValue = adapter.getValue("PEER_PERSISTENT_KEEPALIVE");
+        if (!keepAliveValue.isEmpty()) {
+            peer.keepAlive = Integer.parseInt(keepAliveValue);
+        }
         wireguardSettings.peers.add(peer);
 
         return wireguardSettings;
@@ -80,6 +95,7 @@ public class WireguardActivity extends ProxyActivity<WireguardSettings> {
         hashMap.put("PEER_PUBLIC_KEY", outbound.settings.peers.get(0).publicKey);
         hashMap.put("PEER_PRE_SHARED_KEY", outbound.settings.peers.get(0).preSharedKey);
         hashMap.put("PEER_ALLOWED_IPS", String.join(",", outbound.settings.peers.get(0).allowedIPs));
+        hashMap.put("PEER_PERSISTENT_KEEPALIVE", String.valueOf(outbound.settings.peers.get(0).keepAlive));
 
         return hashMap;
     }
@@ -91,9 +107,9 @@ public class WireguardActivity extends ProxyActivity<WireguardSettings> {
 
     @Override
     protected void initializeInputs(IProxyEditor adapter) {
-        adapter.addInput("ADDRESS1", "Address 1", "", "Local Address (IPv4 CIDR)");
-        adapter.addInput("ADDRESS2", "Address 2", "", "Local Address (IPv6 CIDR)");
-        adapter.addInput(new ProxyEditTextAdapter.TextInputGen("PRIVATE_KEY", "Private Key", "", (view) -> {
+        adapter.addInput("ADDRESS1", getString(R.string.wireguard_address1_label), "", getString(R.string.wireguard_address1_helper));
+        adapter.addInput("ADDRESS2", getString(R.string.wireguard_address2_label), "", getString(R.string.wireguard_address2_helper));
+        adapter.addInput(new ProxyEditTextAdapter.TextInputGen("PRIVATE_KEY", getString(R.string.wireguard_private_key_label), "", (view) -> {
             Key privateKey = Key.generatePrivateKey();
             adapter.setValue("PRIVATE_KEY", privateKey.toBase64());
             adapter.notifyValueChanged("PRIVATE_KEY");
@@ -108,12 +124,13 @@ public class WireguardActivity extends ProxyActivity<WireguardSettings> {
             } catch (KeyFormatException ignored) {
             }
         }));
-        adapter.addInput(new ProxyEditTextAdapter.TextInput("PUBLIC_KEY", "Public Key", "", true));
-        adapter.addInput("RESERVED", "Reserved", "0,0,0", "Format: 0,0,0");
-        adapter.addInput("PEER_ENDPOINT", "Peer Endpoint", "", "Example: engage.cloudflareclient.com:2408");
-        adapter.addInput("PEER_PUBLIC_KEY", "Peer Public Key");
-        adapter.addInput("PEER_PRE_SHARED_KEY", "Peer Pre Shared Key");
-        adapter.addInput("PEER_ALLOWED_IPS", "Allowed IPs", "0.0.0.0/0,::/0");
+        adapter.addInput(new ProxyEditTextAdapter.TextInput("PUBLIC_KEY", getString(R.string.wireguard_public_key_label), "", true));
+        adapter.addInput("RESERVED", getString(R.string.wireguard_reserved_label), "0,0,0", getString(R.string.wireguard_reserved_helper));
+        adapter.addInput("PEER_ENDPOINT", getString(R.string.wireguard_peer_endpoint_label), "", getString(R.string.wireguard_peer_endpoint_helper));
+        adapter.addInput("PEER_PUBLIC_KEY", getString(R.string.wireguard_peer_public_key_label));
+        adapter.addInput("PEER_PRE_SHARED_KEY", getString(R.string.wireguard_peer_pre_shared_key_label));
+        adapter.addInput("PEER_ALLOWED_IPS", getString(R.string.wireguard_allowed_ips_label), "0.0.0.0/0,::/0");
+        adapter.addInput("PEER_PERSISTENT_KEEPALIVE", getString(R.string.wireguard_persistent_keepalive_label), String.valueOf(new WireguardPeer().keepAlive), getString(R.string.wireguard_persistent_keepalive_helper));
     }
 
     /**
